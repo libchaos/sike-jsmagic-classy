0d7fb71821e03b171393b4c6b9996fefb6ef2bb8
libin <libchaos01@163.com>
2016-05-12 16:49:23 +0800

    你好吗

--- classy.js
+++ classy.js
@@ -1,20 +1,56 @@
-function Class(obj){
-  var obj = obj || {};
+function Class(child, parent){
   
-  if(obj && obj.hasOwnProperty('initialize') && (typeof obj['initialize'] === "function")){
-    for(var props in obj){
-      if(obj.hasOwnProperty(props) && props !== "initialize"){
-        obj['initialize'].prototype[props] = obj[props];
+  var child = child || {};
+  var parent = parent || {};
+   
+  function F(){};
+  F.prototype = parent || {};
+  var f = new F(); 
+  
+  if(child && child.hasOwnProperty('initialize') && (typeof child['initialize'] === "function")){
+    child['initialize'].prototype = f;
+    child['initialize'].prototype.constructor = child['initialize'];
+    // child['initialize'].super = parent;
+    for(var props in child){
+      if(child.hasOwnProperty(props) && props !== "initialize"){
+        child['initialize'].prototype[props] = child[props];
       }
     }
-    return obj['initialize'];
-  }else{
-    obj['initialize'] = function(){};
     
-    return obj['initialize'];
+    return child['initialize'];
+  }else{
+    child['initialize'] = function(){};
+    child['initialize'].prototype = f;
+    child['initialize'].prototype.constructor = child['initialize'];
+    // child['initialize'].super = parent
+    for(var props in child){
+      if(child.hasOwnProperty(props) && props !== "initialize"){
+        child['initialize'].prototype[props] = child[props];
+      }
+    }
+
+    return child['initialize'];
   }
  
 }
 
 
-module.exports = Class;
+// module.exports = Class;
+
+
+
+const A = Class({
+  a: function() {
+    return 1;
+  }
+});
+
+const B = Class({
+  b: function() {
+    return 2;
+  }
+}, A);
+
+const b = new B();
+const a = new A();
+
